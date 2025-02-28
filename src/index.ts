2ea7e01f1c23d6011024f1a69cb77b9903091397
root <root@juninho.dev>
2025-02-28 21:36:24 +0000

    add code

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import makeWASocket, { DisconnectReason, useMultiFileAuthState } from 'baileys'
 import { Boom } from '@hapi/boom'
 
+async function sleep (ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 async function connectToWhatsApp () {
     const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys')
     const sock = makeWASocket({
         // can provide additional config here
         auth: state,
-        printQRInTerminal: true
+        printQRInTerminal: true,
     })
-    sock.ev.on('connection.update', (update) => {
+    sock.ev.on('connection.update', async (update) => {
         const { connection, lastDisconnect } = update
         if(connection === 'close') {
             const shouldReconnect = (lastDisconnect?.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut
@@ -19,16 +23,26 @@ async function connectToWhatsApp () {
             }
         } else if(connection === 'open') {
             console.log('opened connection')
-        }
-    })
-    sock.ev.on('messages.upsert', async event => {
-        for (const m of event.messages) {
-            console.log(JSON.stringify(m, undefined, 2))
+        let jid = '555199354299@s.whatsapp.net'
+        const result = await sock.onWhatsApp(jid)
+        console.log (`${jid} exists on WhatsApp, as jid: ${JSON.stringify(result, undefined, 2)}`)
+        await sock.sendMessage('555199354299@s.whatsapp.net'!, { text: 'EAI CARA' })
 
-            console.log('replying to', m.key.remoteJid)
-            await sock.sendMessage(m.key.remoteJid!, { text: 'Hello Word' })
         }
     })
+       
+    // sock.ev.on('messages.upsert', async event => {
+    //     let i = 0;
+    //     for (const m of event.messages) {
+    //         console.log(JSON.stringify(m, undefined, 2))
+    //         await sock.sendMessage('555199354299@s.whatsapp.net', { text: 'Hello Word' })
+    //         i++;
+    //         await sleep(1000);
+    //         if(i > 3) {
+    //             break;
+    //         }
+    //     }
+    // });
 
     // to storage creds (session info) when it updates
     sock.ev.on('creds.update', saveCreds)
